0931b7fe449db48983c96dbb9d325d2d37340f0c
Minh Khoi Nguyen <mngu0115@student.monash.edu>
2024-05-11 12:34:44 +1000

    Feat: Build scaffold for task2

---
+++ task2/main.py
@@ -0,0 +1,18 @@
+from typing import List, Optional, Annotated
+
+
+def allocate(preferences: List[List[int]], officers_per_org: List[List[int]]) -> Optional[Annotated[List[List[List[int]]], 'allocation']]:
+    return None
+
+class Graph:
+    def __init__(self, preferences: List[List[int]], officers_per_org: List[List[int]]):
+        return
+
+    def _add_edge(self):
+        return None
+
+    def _bfs(self, preferences: List[List[int]], officers_per_org: List[List[int]]):
+        return None
+
+    def _edmonds_karp(self):
+        return None
